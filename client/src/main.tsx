0b643f6a71e7259b6be95a88f08da0085dcb8594
mlshvaa <malyshevaa01@mail.ru>
2025-07-04 18:51:10 +0300

    MainPage: semi done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store/store.ts';
+import "./style.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
